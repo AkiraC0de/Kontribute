00f64474faac2bc99cde2ab6d55ae71ca7830a67
AkiraC0de <ravenakira22@gmail.com>
2026-05-05 22:55:19 +0800

    Added required node packages

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("est2ss");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`The Server is running at PORT ${process.env.PORT}`);
+})
