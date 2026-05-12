409b05de9cc08ac1588e92c4cbcad8815c74bbee
AkiraC0de <ravenakira22@gmail.com>
2026-05-12 15:36:00 +0800

    Finished the registerUser service for auth:

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,4 +2,11 @@ import { registerUser } from "../services/auth.services.js"
 
 export const handleRegister = async (req, res) => {
   const result = await registerUser(req.body);
+
+  return res.status(200)
+    .json({
+      success: true,
+      message: "Account has successfully created.",
+      data: result
+    });
 }
--- backend/src/services/auth.services.js
+++ backend/src/services/auth.services.js
@@ -1,14 +1,44 @@
 import GenericError from "../../errors/GenericError.js";
 import ERROR_CODES from "../../config/errorCodes.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const registerUser = async (userData) => {
   const { firstName, lastName, middleInitial, email, password} = userData;
 
-  // Check is the user already exist.
-  const userExist = await User.findOne({email, isEmailVerified: true});
-  if(userExist){
-    throw GenericError(400, "The email has already been registered.", ERROR_CODES.EMAIL_ALREADY_REGISTERED)
+  const existingUser = await User.findOne({email});
+  
+  // Check if the user already exist and is VERIFIED.
+  if(existingUser && existingUser.isEmailVerified){
+    throw new GenericError(400, "The email has already been registered.", ERROR_CODES.EMAIL_ALREADY_REGISTERED)
   }
 
+  // hash the password
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  // If they exist but are UNVERIFIED, overwrite/update their details
+  if(existingUser && !existingUser.isEmailVerified){
+    existingUser.firstName = firstName;
+    existingUser.lastName = lastName;
+    existingUser.middleInitial = middleInitial;
+    existingUser.password = hashedPassword;
+    
+    // Resets the TTL index timer so the account doesn't expire mid-verification
+    existingUser.createdAt = new Date(); 
+
+    await existingUser.save();
+    
+    return existingUser.toPublicJSON();
+  }
+
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    middleInitial,
+    email,
+    password: hashedPassword
+  });
+
+  return newUser.toPublicJSON();
 }
