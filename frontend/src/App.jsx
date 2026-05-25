c1c4d230890b1f716e99099ffe718b1ac8941ff1
AkiraC0de <ravenakira22@gmail.com>
2026-05-25 20:27:35 +0800

    frontend: reconstructed the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router";
 import Landing from "./pages/Landing";
-import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
-import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/auth/:state" element={<Auth />} />
       
-      <Route element={<ProtectedRoute/>}>
-        <Route path="/home" element={<Home />}/>
-      </Route>
+      <Route path="/home" element={<Home />}/>
 
       <Route path="*" element={<NotFound />} />
     </Routes>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 import "./index.css";
 import App from "./App.jsx";
-import AuthProvider from "./providers/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <Provider store={store}>
         <App />
-      </AuthProvider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
